cf9e204267d9e2a279caf17ce1d2bc38f6a6ec01
Impanda9 <137157462+Impanda9@users.noreply.github.com>
2024-01-23 08:34:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function saveAnswer() {
     var answer = document.getElementById('textInput').value;
     localStorage.setItem('VideoAnswer', answer);
-    window.location.href = https://impanda9.github.io/4cols/;
+    window.location.href = 'https://impanda9.github.io/4cols/';
 }
